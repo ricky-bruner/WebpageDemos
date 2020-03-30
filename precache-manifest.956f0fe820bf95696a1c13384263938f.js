361610cfa6969454a8e25d631346ec151e4f7133
ricky-bruner <alteredecho07@yahoo.com>
2020-03-30 01:27:46 -0500

    Updates

---
+++ precache-manifest.956f0fe820bf95696a1c13384263938f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df11c6781b117d72452501ee45988d23",
+    "url": "/WebpageDemos/index.html"
+  },
+  {
+    "revision": "54790d1b01751c79c3b6",
+    "url": "/WebpageDemos/static/css/main.e025e208.chunk.css"
+  },
+  {
+    "revision": "c868e8905ea9f000a523",
+    "url": "/WebpageDemos/static/js/2.2bdd7687.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/WebpageDemos/static/js/2.2bdd7687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54790d1b01751c79c3b6",
+    "url": "/WebpageDemos/static/js/main.ea98d302.chunk.js"
+  },
+  {
+    "revision": "526a972811b26de68eb7",
+    "url": "/WebpageDemos/static/js/runtime-main.f3ca1b71.js"
+  },
+  {
+    "revision": "b38c469a630e6cda06db2501d1c4c231",
+    "url": "/WebpageDemos/static/media/ProjectLogo1.b38c469a.png"
+  },
+  {
+    "revision": "9267e30203ba30b605c192347f411d69",
+    "url": "/WebpageDemos/static/media/ProjectLogo2.9267e302.png"
+  },
+  {
+    "revision": "7687f6510c48edf2a66c73419d5fc871",
+    "url": "/WebpageDemos/static/media/ProjectLogo3.7687f651.png"
+  },
+  {
+    "revision": "accac18ad2b94c08598741c7ec522438",
+    "url": "/WebpageDemos/static/media/dumbledore.accac18a.jpg"
+  },
+  {
+    "revision": "f24e493f2e7f860a12f716222267cf2e",
+    "url": "/WebpageDemos/static/media/hagrid.f24e493f.jpg"
+  },
+  {
+    "revision": "dacf1007a80457b94023b1921f5e74de",
+    "url": "/WebpageDemos/static/media/quote.dacf1007.svg"
+  },
+  {
+    "revision": "7ee55dd92a837c28f9c380310d78f4f6",
+    "url": "/WebpageDemos/static/media/snape.7ee55dd9.jpg"
+  }
+]);
