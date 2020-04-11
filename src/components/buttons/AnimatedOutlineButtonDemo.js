ebce26dff0003beb2229f59fda03f5d093449d4f
ricky-bruner <alteredecho07@yahoo.com>
2020-04-11 01:46:37 -0500

    added animated outline button demo

---
+++ src/components/buttons/AnimatedOutlineButtonDemo.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+
+export default class AnimatedOutlineButtonDemo extends Component {
+    state = {};
+
+    render(){
+        return (
+            <div id="animated-outline-button-demo" className="animated-outline-button-demo">
+                <h2>Animated Outline Buttons</h2>
+                <div className="container">
+                    <div className="button-container" data-text="X-Large">
+                        <a href="animated-outline-button-demo" className="animated-outline-button xlg">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button xlg">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button xlg">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                    </div>
+                    <div className="button-container" data-text="Large">
+                        <a href="animated-outline-button-demo" className="animated-outline-button lg">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button lg">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button lg">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                    </div>
+                    <div className="button-container" data-text="Medium">
+                        <a href="animated-outline-button-demo" className="animated-outline-button md">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button md">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button md">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                    </div>
+                    <div className="button-container" data-text="Small">
+                        <a href="animated-outline-button-demo" className="animated-outline-button sm">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button sm">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button sm">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button sm">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                    </div>
+                    <div className="button-container" data-text="X-Small">
+                        <a href="animated-outline-button-demo" className="animated-outline-button xs">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button xs">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button xs">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button xs">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                        <a href="animated-outline-button-demo" className="animated-outline-button xs">
+                            <svg>
+                                <rect></rect>
+                            </svg>
+                            Button
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
