a6a0deec5b46c7030d39c57d8ad3ec4de7ef583b
ricky-bruner <alteredecho07@yahoo.com>
2020-08-10 23:59:08 -0500

    cited all testimonials

--- src/modules/testimonials/TestimonialDemoCitationsFactory.js
+++ src/modules/testimonials/TestimonialDemoCitationsFactory.js
@@ -5,13 +5,13 @@ import CitationSection from "../../components/app/demos/CitationSection";
 export default function TestimonialDemoCitationsFactory(demoName){
     switch(demoName){
         case TestimonialNames.QuoteMovementTestimonials:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=b4sG5h_Coa4" embeddedLink="https://www.youtube.com/embed/b4sG5h_Coa4" />
 
         case TestimonialNames.ColorfulHoverQuoteBoxTestimonials:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=aK7RQEY30lo" embeddedLink="https://www.youtube.com/embed/aK7RQEY30lo" />
 
         case TestimonialNames.BottomColorRiseTestimonials:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=pGEcBBjvRlU" embeddedLink="https://www.youtube.com/embed/pGEcBBjvRlU" />
 
         default:
             return null;
