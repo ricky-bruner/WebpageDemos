5c70763311ee067eb35ea44b0f1517b41df485bd
ricky-bruner <alteredecho07@yahoo.com>
2020-08-11 00:08:56 -0500

    cited all button demos

--- src/modules/buttons/ButtonDemoCitationsFactory.js
+++ src/modules/buttons/ButtonDemoCitationsFactory.js
@@ -5,25 +5,25 @@ import CitationSection from '../../components/app/demos/CitationSection';
 export default function ButtonDemoCitationsFactory(demoName){
     switch(demoName){
         case ButtonNames.LiquidWaveButtons:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=wjHTKLstbRg" embeddedLink="https://www.youtube.com/embed/wjHTKLstbRg" />
 
         case ButtonNames.NeonReflectiveButtons:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=3RRgVHd2TXQ" embeddedLink="https://www.youtube.com/embed/3RRgVHd2TXQ" />
 
         case ButtonNames.ThreeDimensionalButtons:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=nYHEmB6xalo" embeddedLink="https://www.youtube.com/embed/nYHEmB6xalo" />
 
         case ButtonNames.OscillatingSolidButtons:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=SaWuDZZCkw8" embeddedLink="https://www.youtube.com/embed/SaWuDZZCkw8" />
 
         case ButtonNames.AnimatedOutlineButtons:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=KT6YusE_7dk" embeddedLink="https://www.youtube.com/embed/KT6YusE_7dk" />
 
         case ButtonNames.ShootingBorderButtons:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=w0V4HAYYCbU" embeddedLink="https://www.youtube.com/embed/w0V4HAYYCbU" />
 
         case ButtonNames.MiddleOutButtons:
-            return <CitationSection externalLink="" embeddedLink="" />
+            return <CitationSection externalLink="https://www.youtube.com/watch?v=CzedjonaEjc" embeddedLink="https://www.youtube.com/embed/CzedjonaEjc" />
 
         default:
             return null;
