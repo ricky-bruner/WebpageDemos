6652cc26c95b03fff09cf8384e692bd61356ce85
ricky-bruner <alteredecho07@yahoo.com>
2020-11-15 20:40:41 -0600

    Updates

---
+++ precache-manifest.2aaf3e0831e1a00c54f58dae793e43a5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e27f775ecfb394c4b1ab65f7b52aa4",
+    "url": "/www.transfigure.io/index.html"
+  },
+  {
+    "revision": "37d0b8366f161e2b95d3",
+    "url": "/www.transfigure.io/static/css/main.12dd1ba3.chunk.css"
+  },
+  {
+    "revision": "f94d22aeaa5e6e3d0426",
+    "url": "/www.transfigure.io/static/js/2.d4931d48.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/www.transfigure.io/static/js/2.d4931d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d0b8366f161e2b95d3",
+    "url": "/www.transfigure.io/static/js/main.35079072.chunk.js"
+  },
+  {
+    "revision": "30d8dcb02a5c410c4e75",
+    "url": "/www.transfigure.io/static/js/runtime-main.4a8d6606.js"
+  },
+  {
+    "revision": "49eb486c97b07331a13cba3d20403b45",
+    "url": "/www.transfigure.io/static/media/HowToUse.49eb486c.png"
+  },
+  {
+    "revision": "b38c469a630e6cda06db2501d1c4c231",
+    "url": "/www.transfigure.io/static/media/ProjectLogo1.b38c469a.png"
+  },
+  {
+    "revision": "9267e30203ba30b605c192347f411d69",
+    "url": "/www.transfigure.io/static/media/ProjectLogo2.9267e302.png"
+  },
+  {
+    "revision": "7687f6510c48edf2a66c73419d5fc871",
+    "url": "/www.transfigure.io/static/media/ProjectLogo3.7687f651.png"
+  },
+  {
+    "revision": "35c115dbc29f0373c44f41f33d38a49c",
+    "url": "/www.transfigure.io/static/media/dumbledore-portrait.35c115db.png"
+  },
+  {
+    "revision": "accac18ad2b94c08598741c7ec522438",
+    "url": "/www.transfigure.io/static/media/dumbledore.accac18a.jpg"
+  },
+  {
+    "revision": "d57f3f763e80e51b2f691e60703b3a6f",
+    "url": "/www.transfigure.io/static/media/hagrid-portrait.d57f3f76.png"
+  },
+  {
+    "revision": "f24e493f2e7f860a12f716222267cf2e",
+    "url": "/www.transfigure.io/static/media/hagrid.f24e493f.jpg"
+  },
+  {
+    "revision": "1f6692fd1fb2e944b56fe008b01c4986",
+    "url": "/www.transfigure.io/static/media/online-tutorials-icon.1f6692fd.png"
+  },
+  {
+    "revision": "dacf1007a80457b94023b1921f5e74de",
+    "url": "/www.transfigure.io/static/media/quote.dacf1007.svg"
+  },
+  {
+    "revision": "41111c892ae327b858e5abf50446e242",
+    "url": "/www.transfigure.io/static/media/right-arrow.41111c89.png"
+  },
+  {
+    "revision": "6a1f132843c3b2509125517148b18c61",
+    "url": "/www.transfigure.io/static/media/snape-portrait.6a1f1328.png"
+  },
+  {
+    "revision": "7ee55dd92a837c28f9c380310d78f4f6",
+    "url": "/www.transfigure.io/static/media/snape.7ee55dd9.jpg"
+  },
+  {
+    "revision": "c5a4ee95040d54e40e539b03341d1cf3",
+    "url": "/www.transfigure.io/static/media/transfigure_io_transparent_code_blue.c5a4ee95.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/transfigure.io/precache-manifest.a99ec974e0a0176e98b7597099a8fcd9.js"
+  "/www.transfigure.io/precache-manifest.2aaf3e0831e1a00c54f58dae793e43a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/transfigure.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.transfigure.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
